a73ecf3de35d6890d134e32867cb0ffad941fa85
alejox <alejoxdrums@gmail.com>
2023-04-04 23:17:08 -0500

    updates

---
+++ src/views/PokemonPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>¿Quién es este Pokémon?</h1>
+  <PokemonPicture :pokemonId="412" :showPokemon="true" />
+  <PokemonOptions />
+</template>
+
+<script setup>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
+import getPokemonOptions from '@/helpers/getPokemonOptions';
+
+console.log(getPokemonOptions());
+</script>
